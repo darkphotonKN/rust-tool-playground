ea7c6df87a73daee99f145c90926cb7efd7e77da
Kranti <kranti@Krantis-MacBook-Pro.local>
2023-11-07 22:40:56 +0800

    Adding memory manipulation and vectors

---
+++ src/arrays.rs
@@ -0,0 +1,6 @@
+
+pub fn addToArray(num: u32, mut arr: Vec<u32>) -> Vec<u32> {
+  arr.push(num);
+  arr
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 
 mod my_funcs;
 use crate::my_funcs::sum;
+mod arrays;
+use crate::arrays::addToArray;
 
 fn main() {
     let mut x: u32 = 3;
@@ -11,4 +13,12 @@ fn main() {
     // using sum function to add 1 and 3
     let y: u32 = sum(1, 3);
     println!("result: {}", y);
+
+    // adding to a vector array
+    let mut arr: Vec<u32> = vec![1,2,3]; // dynamic array 
+    let mut arr2: Vec<u32>;
+
+    arr2 = addToArray(4, arr);
+    println!("arr2: {:?}", arr2);
+    
 }

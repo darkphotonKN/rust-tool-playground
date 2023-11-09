a5fd53482e4498158ee44fa53d34971adbc870c4
Kranti <kranti@Krantis-MacBook-Pro.local>
2023-11-10 01:46:03 +0800

    Adding a few rules and coding best practices

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,25 @@ fn main() {
     let mut arr: Vec<u32> = vec![1,2,3]; // dynamic array 
     let mut arr2: Vec<u32>;
 
+    //*** Borrowing 
+    
+    // this does NOT work as value is borrowed
+    // let n: String = String::from("tester");
+    // let m: String = n;
+    // println!("n: {}", n);
+
+    // this works, type needs to be a reference along with the value needs to be marked as 
+    // a reference (with the & symbol)
+    let mut n: String = String::from("tester");
+    let m: &mut String = &mut n;
+    n.push('?');
+    println!("n: {}", n);
+
     arr2 = addToArray(4, arr);
     println!("arr2: {:?}", arr2);
+
+    for i in 0..2 {
+        println!("looping i:{}", i)
+    }
     
 }
